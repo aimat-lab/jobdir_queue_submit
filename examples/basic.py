dec807fd5196a157d28973fa9a9f2de0f0320b36
Patrick Reiser <64005383+PatReis@users.noreply.github.com>
2021-02-27 20:32:08 +0100

    Add files via upload

--- examples/basic.py
+++ examples/basic.py
@@ -1,50 +1,36 @@
-"""
-Basic example of possible usage
-@author: Patrick Reiser
-"""
-
 from mjdir.MultiJobDirectory import MultiJobDirectory
 import os
+import time
 
 #Example for basic usage
 jobdir = MultiJobDirectory("TestJobs")
 
 # Adding jobdirectories
-jobdir.add("job_1",command = 'echo "simple job" > test.txt\n') # if command is not None, this command will be used in run()
+jobdir.add({"job_1" : {'command' : 'echo "simple job" > {path}/test.txt\n' , 'input' : "coord"}}) # if command is not None, this command will be used in run()
 jobdir.add(["job_2","job_3","job_4","job_5"])
-#jobdir.remove("job_3")
+
 
 # Get path of jobdirectory
-print("Path of job 2:",jobdir.get("job_2",full_path=True))
+print("Path of job 1:",jobdir.get("job_1"))
 
 #Typical input generation
 def fun_write_input_example(path):
     with open(os.path.join(path,"inputfile.txt"),"w") as f:
         f.write("atom here")
 #Write input
-fun_write_input_example(jobdir.get("job_1"))
+fun_write_input_example(jobdir.get()["job_1"]['path'])
 
-#Ddefine slurm settings
-jobdir.slurm_header = "cd $SLURM_SUBMIT_DIR\n#module load something\n"
-jobdir.slurm_tasks = "2"
-jobdir.slurm_time = "30:00:00"
-#jobdir.slurm_partition = 'normal'  # For for-hlr need partition to run slurm
+
+submit_properties = {'-p':'normal'} # For for-hlr need partition to run slurm
 
 #Submit jobs for array, procs = 2 number of jobs to submit
-submits = jobdir.run(procs=2,command='echo "default command" > test.txt\n',scripts_only=False,ignore_running=True) # jobs=0 means all
+submits = jobdir.run(procs=2,
+                     command='echo "default command" > {path}/test.txt\n',
+                     header = "cd $SLURM_SUBMIT_DIR\n#module load something\n",
+                     submit_properties = {}, # for int-nano
+                     ) 
 print("Slurm submitted:",submits)
 
-#Check for running jobs
-runjobs = jobdir.check(check_logs = False)
-print("Jobs running: ",runjobs)
-
-#Copy file from jobdir, will be done by public function in the future
-jobdir._copy_files_from_dir(jobdir.get("job_1"),".",".txt")
-
-#Wait/check for a specific job to finish
-jobdir.wait(updates=0.2)
-runjobs = jobdir.check(check_logs = True)
-print("Jobs running: ",runjobs)
 
 #Read output via custom function
 def fun_test_success(path):
@@ -56,6 +42,10 @@ def fun_test_success(path):
             return out
     else:
         return out
+
+
+print("Waiting 10s ...")
+time.sleep(10)
     
 #Use get() or by evaluate(function)
-print("Readout: ",jobdir.evaluate(fun_test_success))
+print(fun_test_success(jobdir.get()['job_1']['path']))
--- examples/turbomole_ase.py
+++ examples/turbomole_ase.py
@@ -1,13 +1,8 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Mar 14 11:38:49 2020
-
-@author: Patrick
-"""
 import os
 import numpy as np
+import time
 from mjdir.MultiJobDirectory import MultiJobDirectory
-from mjdir.commands.JOBDIRturbomole import TURBOMOLE_SLURM_HEADERS,TURBOMOLE_SLURM_COMMANDS,write_turbomole_input,read_turbomole_output,read_turbomole_eiger_file
+from mjdir.commands.turbomole import TURBOMOLE_SLURM_HEADERS,TURBOMOLE_SLURM_COMMANDS,write_turbomole_input,read_turbomole_output,read_turbomole_eiger_file
 
 from ase import Atoms
 from ase.calculators.turbomole import Turbomole
@@ -15,6 +10,10 @@ from ase.calculators.turbomole import Turbomole
 # Important for this python module turbomole module has to be loaded externally                
 print("Turbomole must be available by module load chem/turbomole for ase input")
 
+
+jobs = ["H2","N2"]
+cord = [[[0.0,0.0,0.0],[0.0,0.0,0.8]],[[0.0,0.0,0.0],[0.0,0.0,1.4]]]
+elem = [["H","H"],["N","N"]]
 
 params = {
     'use resolution of identity': True,   
@@ -26,31 +25,59 @@ params = {
 
 
 jd = MultiJobDirectory("Turbo_Example")
-jd.slurm_header = TURBOMOLE_SLURM_HEADERS["int-nano"]
-jd.slurm_tasks = "10"
-jd.slurm_time = "30:00:00"
-#jobdir.slurm_partition = 'normal'  # For for-hlr need partition to run slurm
 
-jlist = ["H2","N2"]
-cord = [[[0.0,0.0,0.0],[0.0,0.0,0.8]],[[0.0,0.0,0.0],[0.0,0.0,1.4]]]
-elem = [["H","H"],["N","N"]]
 
-for i in range(0,len(jlist)):
-    jd.add(jlist[i])
+slurm_params = { 'tasks' : "10",
+                # "ntasks-per-node" : "10",
+                'time' : "30:00:00",
+                'nodes' : "1"
+}
+
+
+submit_properties = {'-p':'normal'} # For for-hlr need partition to run slurm
+
+
+for i in range(0,len(jobs)):
+    jd.add(jobs[i])
     at = Atoms(elem[i],cord[i])
     calc = Turbomole(**params)
-    write_turbomole_input(jd.get(jlist[i]),calc,at)
+    write_turbomole_input(jd.get()[jobs[i]]['path'],calc,at)
 
-runnjobs = jd.run(procs=2,command=TURBOMOLE_SLURM_COMMANDS["int-nano"]["energy"],ignore_running=True)
-jd.check()
-jd.wait()
-    
+jd.save()
+
+print("submit ridft")
+runnjobs = jd.run(procs=1,
+                  asyn=2,
+                  command= TURBOMOLE_SLURM_COMMANDS["energy"],
+                  header = TURBOMOLE_SLURM_HEADERS["int-nano"],
+                  queue_properties = slurm_params,
+                  submit_properties = {}, # for int-nano
+                  prepare_only=False)
+
+print("Waiting 10s ...")
+time.sleep(10)
+
+print("submit readout")
+runnjobs = jd.run(procs=2,
+                  command= TURBOMOLE_SLURM_COMMANDS["eiger"],
+                  header = TURBOMOLE_SLURM_HEADERS["int-nano"],
+                  queue_properties = slurm_params,
+                  submit_properties = {}, # for int-nano
+                  prepare_only=False)
+
+print("Waiting 10s ...")
+time.sleep(10)
+
+# Check readout    
 homos = []
 lumos = []
-for i in range(0,len(jlist)):
-    hm,lm = read_turbomole_eiger_file(jd.get(jlist[i]))
-    homos.append(hm)
-    lumos.append(lm)
+for i in range(0,len(jobs)):
+    try:
+        hm,lm,eng = read_turbomole_eiger_file(jd.get()[jobs[i]]['path'])
+        homos.append(hm)
+        lumos.append(lm)
+    except:
+        print("jobs not finished yet or output bad.")
 
-np.save("HOMO_"+jd.dirname+".npy",np.array(homos))
-np.save("LUMO_"+jd.dirname+".npy",np.array(lumos))
+print("homos:", homos)
+print("lumos:", lumos)
